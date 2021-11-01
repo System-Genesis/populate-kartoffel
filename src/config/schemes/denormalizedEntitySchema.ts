9ba0785a27ff3e6f1281572ebdd9b8d04930c19f
NoamYes <noamiko600@gmail.com>
2021-11-02 00:46:10 +0200

    (fix): change clearance to string

--- src/config/schemes/denormalizedEntitySchema.ts
+++ src/config/schemes/denormalizedEntitySchema.ts
@@ -17,7 +17,7 @@ export default new Schema<DenormalizedEntity, Model<DenormalizedEntity>, Denorma
     rank: String, 
     phone: [String],
     address: String,
-    clearance: Number, 
+    clearance: String, 
     sex: String,
     birthDate: Date,
     goalUserId: String,
