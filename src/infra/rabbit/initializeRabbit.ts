9a621e9cfe4521796eac673ca297254af95236a6
hagai245 <chagaimillot@gmail.com>
2021-11-22 16:19:55 +0200

    הפעם באמת

--- src/infra/rabbit/initializeRabbit.ts
+++ src/infra/rabbit/initializeRabbit.ts
@@ -2,7 +2,7 @@ import menash from "menashmq";
 import config from "../../config/index";
 
 export const initializeRabbit = async () => {
-  await menash.connect(config.rabbit.uri);
+  await menash.connect(config.rabbit.uri, {forever: true});
   await menash.declareTopology({
     queues: [
       {
